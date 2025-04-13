d8e2bde1176b660dd674fd2846202a2abb89eead
tioluwa <tioluwa.dev@gmail.com>
2025-04-13 23:06:49 +0100

    Fixes

--- generics/utils/db.py
+++ generics/utils/db.py
@@ -6,10 +6,14 @@ def get_database_url(
     db_host: str,
     db_port: str,
     db_driver: str,
+    **kwargs,
 ) -> str:
     """
     Returns a DB url based on the given parameters
     """
-    return (
-        f"{db_type}+{db_driver}://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+    query_string = (
+        "?" + "&".join(f"{key}={value}" for key, value in kwargs.items())
+        if kwargs
+        else ""
     )
+    return f"{db_type}+{db_driver}://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}{query_string}"
